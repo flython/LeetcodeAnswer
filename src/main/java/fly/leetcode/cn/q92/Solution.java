4c55e8b0f009f525259e7e84291e9f540ada9a5f
Flython <vipxyz@vip.qq.com>
2021-03-19 00:52:57 +0800

    今天的每日一题又是人生浪费题

--- src/main/java/fly/leetcode/cn/q92/Solution.java
+++ src/main/java/fly/leetcode/cn/q92/Solution.java
@@ -1,32 +1,35 @@
 package fly.leetcode.cn.q92;
 
+import java.util.List;
+
 /**
  * 1. 注意left right的大小关系，边界值，1 ≤ m ≤ n ≤ 链表长度。
  * 2. 范围从1开始
+ * 原题目说只能遍历一次
+ *
+ * 额...我拿数组来保存不知道算不算遍历一次
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        ListNode top = head;
-        ListNode tmp = top.next;
-        // left == 1 时不会移动
+        ListNode[] arr = new ListNode[right-left+1];
+
+        ListNode p = head;
         for (int i = 1; i < left; i++) {
-            top = top.next;
-            tmp = top.next;
+            p = p.next;
         }
 
-        for (int i = right - left - 2; i >= 0; i--) {
-            if (i == 0){
-                //由于不能翻转第一个Node，所以相互赋值
-                var tmpVal = tmp.next.val;
-                tmp.next.val = top.val;
-                top.val = tmpVal;
-                break;
-            }
-            var cur = tmp;
-            tmp = tmp.next;
-            var next = tmp.next;
+        for (int i = 0; i <= right-left ; i++) {
+            arr[i] = p;
+            p = p.next;
         }
-        top.next = tmp;
+
+        for (int i = 0; i < arr.length/2; i++) {
+            ListNode switcher = arr[arr.length - 1 - i];
+            arr[i].val = arr[i].val ^ switcher.val;
+            switcher.val = arr[i].val ^ switcher.val;
+            arr[i].val = arr[i].val ^ switcher.val;
+        }
+
         return head;
     }
 }
--- src/main/java/fly/leetcode/cn/q92/Solution2.java
+++ src/main/java/fly/leetcode/cn/q92/Solution2.java
@@ -1,35 +1,37 @@
 package fly.leetcode.cn.q92;
 
 /**
- * 1. 注意left right的大小关系，边界值，1 ≤ m ≤ n ≤ 链表长度。
- * 2. 范围从1开始
- * 新建对象解法
+ * 网上找的，报错了
  */
 class Solution2 {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        ListNode top = head;
-        ListNode tmp = top.next;
-        // left == 1 时不会移动
-        for (int i = 1; i < left; i++) {
-            top = top.next;
-            tmp = top.next;
+        int len = 0;
+        ListNode node1 = null;
+        ListNode pre = null;
+        ListNode last = null;
+        while(node1!=null){
+            len++;
+            pre = len==left-1?node1:pre;   //如果在len++的过程中，找到了from前一个，那就将pre定为此时累加到的这个节点
+            last = len==right+1?node1:last;    //同理，如果在len++过程中找到了to后面的一个，就赋值为当前点，否则均为null
+            node1 = node1.next;
         }
-        ListNode reverse = null;
-        ListNode tail = null;
-
-        for (int i = right - left - 2; i >= 0; i--){
-            reverse = new ListNode(tmp.val,reverse);
-            if (tail == null){
-                tail = reverse;
-            }
-            if (i == 0){
-                top.val = tmp.val;
-                if (tail != null){
-                    tail = tmp.next;
-                }
-            }
-            tmp = tmp.next;
+        if(left>right||left<1||right>len) {
+            return head;
         }
-        return head;
+        node1 = pre==null?head:pre.next;    //看是否需要换头，如果pre==null的时候，头结点要换了，就是从原头开始换
+        ListNode node2 = node1.next;
+        node1.next = last;       //将原来的头连至to+1
+        ListNode next = null;
+        while(node2!=last) {     //下一位依次指向前
+            next = node2.next;
+            node2.next = node1;
+            node1 = node2;
+            node2 = next;
+        }
+        if(pre!=null) {          //如果pre存在的时候，接到pre（left-1）后面
+            pre.next = node1;
+            return head;
+        }
+        return node1;     //否则不
     }
 }
