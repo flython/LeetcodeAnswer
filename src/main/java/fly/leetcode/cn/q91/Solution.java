1431897bc9e72669cea6e5b768dd095977635249
laizhenhui <laizh@mashang.cn>
2021-04-21 20:28:58 +0800

    [91. 解码方法] 算了，回去再测试

---
+++ src/main/java/fly/leetcode/cn/q91/Solution.java
@@ -0,0 +1,41 @@
+package fly.leetcode.cn.q91;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+class Solution {
+    public int numDecodings(String s) {
+        //处理前导0
+        if (s.startsWith(".")){
+            return 0;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        Map<String,Integer> markbook = new HashMap<>();
+        markbook.put("",1);
+        char[] chars = s.toCharArray();
+        //first must not be 0
+        sb.append(chars[0]);
+        markbook.put(sb.toString(),1);
+        for (int i = 1; i < chars.length; i++) {
+            //如果是0，那么只能与上一个字符连接(假设输入一定满足的情况下)，所以数量等于 n[i] = n[i-2]
+            if (chars[i] == '0'){
+                sb.append(chars[i]);
+                markbook.put(sb.toString(),markbook.get(sb.substring(0,i-1)));
+            } else {
+                //单独作为数字
+                int sum = markbook.get(sb.toString());
+                //与上一个字符连接
+                if (sb.charAt(i-1) == '1' || sb.charAt(i-1) == '2'){
+                    sum += markbook.get(sb.substring(0,i-1));
+                }
+                sb.append(chars[i]);
+            }
+            //如果是3-9，那么有两种方案， 1 与上一个字符连接（如果上一个为1或2）（n[i-2]），2 单独作为字符（n[i-1]），n[i] = n[i-1]+n[i-2]
+            //如果是1,2，
+        }
+
+        return markbook.get(s);
+    }
+}
---
+++ src/test/java/fly/leetcode/cn/q91/SolutionTest.java
@@ -0,0 +1,10 @@
+package fly.leetcode.cn.q91;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @org.junit.jupiter.api.Test
+    void numDecodings() {
+    }
+}
